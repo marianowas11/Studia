6a5f6c8fd86df49d29540c169c7f381ba64ec57f
marianowas11 <46400906+marianowas11@users.noreply.github.com>
2022-01-09 18:05:05 +0100

    v1.1

--- Programowanie_wspolbiezne/LAB/1/zadanko_1/src/com/company/Helper.java
+++ Programowanie_wspolbiezne/LAB/1/zadanko_1/src/com/company/Helper.java
@@ -1,12 +1,12 @@
 package com.company;
 
-import javax.swing.text.html.ListView;
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 
 public class Helper {
 
+    
     public String crateFormatedMenu(List<Pizza> pizzas){
         return pizzas.stream().parallel()
                 .map(
@@ -16,6 +16,16 @@ public class Helper {
                         .collect(Collectors.joining(", ")) + " - " +
                     p.getIngredients().stream().mapToInt(i->i.getPrice()).sum())
                 .collect(Collectors.joining("\n"));
+    }
+    public String crateFormatedMenuStream(List<Pizza> pizzas){
+            return pizzas.stream()
+            .map(
+                p->p.getName() + ": " +
+                p.getIngredients().stream()
+                    .map(i->i.getName())
+                    .collect(Collectors.joining(", ")) + " - " +
+                p.getIngredients().stream().mapToInt(i->i.getPrice()).sum())
+            .collect(Collectors.joining("\n"));
     }
 
     public Pizza findCheapestThinVegatarian(List<Pizza> pizzas){
@@ -33,7 +43,30 @@ public class Helper {
                 .orElse(null);
     }
 
+    public Pizza findCheapestThinVegatarianStream(List<Pizza> pizzas){
+        return pizzas.stream()
+                .filter(p->p.getIngredients().stream()
+                    .noneMatch(i->i.isMeat() && i.equals(Ingredient.THICK_CRUST)))
+                .min((Pizza p1, Pizza p2) ->
+                        p1.getIngredients()
+                                .stream()
+                                .mapToInt(Ingredient::getPrice)
+                                .sum() -
+                                p2.getIngredients()
+                                        .stream()
+                                        .mapToInt(Ingredient::getPrice).sum())
+                .orElse(null);
+     }
+    
     public List<Pizza> withoutAllergen(List<Pizza> pizzas, List<Ingredient> allergens){
+        return pizzas.parallelStream().filter(p->p.getIngredients().stream()
+                .map(i->i.getName())
+                .noneMatch(allergens.stream()
+                        .map(a->a.getName())
+                        .collect(Collectors.toSet())
+                        ::contains)).collect(Collectors.toList());
+    }
+    public List<Pizza> withoutAllergenStream(List<Pizza> pizzas, List<Ingredient> allergens){
         return pizzas.stream().filter(p->p.getIngredients().stream()
                 .map(i->i.getName())
                 .noneMatch(allergens.stream()
@@ -43,14 +76,23 @@ public class Helper {
     }
 
     public Map<String, Set<Ingredient>> groupByIngredients(List<Pizza> pizzas){
+        return pizzas.parallelStream()
+                .flatMap(p ->p.getIngredients().stream()
+                .distinct())
+                .collect(Collectors.groupingBy(
+                        i->i.isSpicy() && i.isMeat() ? "\nSPICY_MEAT"
+                                : i.isMeat() ? "\nMEAT"
+                                : i.isSpicy() ? "\nSPICY"
+                                : "\nOTHER", Collectors.toSet()));
+    }
+    public Map<String, Set<Ingredient>> groupByIngredientsStream(List<Pizza> pizzas){
         return pizzas.stream()
                 .flatMap(p ->p.getIngredients().stream()
                 .distinct())
                 .collect(Collectors.groupingBy(
-                        i->i.isSpicy() && i.isMeat() ? "SPICY_MEAT"
-                                : i.isMeat() ? "MEAT"
-                                : i.isSpicy() ? "SPICY"
-                                : "OTHER", Collectors.toSet()));
-
+                        i->i.isSpicy() && i.isMeat() ? "\nSPICY_MEAT"
+                                : i.isMeat() ? "\nMEAT"
+                                : i.isSpicy() ? "\nSPICY"
+                                : "\nOTHER", Collectors.toSet()));
     }
 }
--- Programowanie_wspolbiezne/LAB/1/zadanko_1/src/com/company/Main.java
+++ Programowanie_wspolbiezne/LAB/1/zadanko_1/src/com/company/Main.java
@@ -3,15 +3,34 @@ package com.company;
 import java.util.*;
 import static com.company.Ingredient.*;
 import static com.company.Pizza.*;
+import java.time.Duration;
+import java.time.Instant;
 
 public class Main {
-    public static final List<Pizza> ALL = Arrays.asList(MARGHERITA, CAPRI, HAVAI, CARUSO, MAMA_MIA, SOPRANO, CALABRESE, VEGETARIANA, CAPRESE, PASCETORE, FOUR_CHEESE, TABASCO, AMORE, FARMER);
+    
+
+    public static final List<Pizza> ALL = Arrays.asList(MARGHERITA, CAPRI, HAVAI,
+            CARUSO, MAMA_MIA, SOPRANO, CALABRESE, VEGETARIANA, CAPRESE, PASCETORE,
+            FOUR_CHEESE, TABASCO, AMORE, FARMER);
 
     public static void main(String[] args) {
-
+        Instant t1,t2;
         Helper helper = new Helper();
-        System.out.println("\nLadne MENU");
+
+        t1 = Instant.now();
+        System.out.println("\nLadne MENU Parallel Stream");
         System.out.println(helper.crateFormatedMenu(ALL));
+        t2 = Instant.now();
+        Duration dt=Duration.between(t1,t2);
+        System.out.println("Czas: " + dt.toMillis() + " milisec\n");
+        t1 = Instant.now();
+        System.out.println("\nLadne MENU Stream");
+        System.out.println(helper.crateFormatedMenuStream(ALL));
+        t2 = Instant.now();
+        dt=Duration.between(t1,t2);
+        System.out.println("Czas: " + dt.toMillis() + " milisec\n");
+
+        
         System.out.println("\nNajtansza CIENKA VEGE pizza");
         System.out.println(helper.findCheapestThinVegatarian(ALL));
 
